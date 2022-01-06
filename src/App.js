f3dfd94aeb8d6f8828819d3b542b87e427448891
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-06 12:54:23 +0100

    functional Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Welcome1, Welcome2 } from './components/Welcome'
+import  Welcome from './components/Welcome'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <Welcome/>
+        <Welcome1/>
+        <Welcome2/>
     </div>
   );
 }
---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+
+
+function Welcome(){
+  return <div>
+            <h1>
+                salam alikoum
+            </h1>
+        </div>
+}
+export default Welcome;
+
+
+export const Welcome1=()=><div><h1>salam alikoum1</h1></div>
+export const Welcome2=()=><div><h1>salam alikoum2</h1></div>
