3b27591d79bd473f8a0b11e0d1c139548b249913
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-06 18:35:21 +0100

    Binding Event Handlers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
-import Hello from './components/Hello';
+
 import Greet from './components/Greet';
 
 function App() {
   return (
     <div className="App">
          <Greet ></Greet>
-         <Hello ></Hello>
+       
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,17 +1,128 @@
 import React from "react";
 
 
+// ================First Methode===========================================
+// class Greet extends React.Component {
+//          constructor(){
+//            super()
+//            this.state={
+//              message:'fisrt message'
+//            }
+//          }
+//   clickHandler(){
+//     this.setState({
+//       message:'last Message'
+//     })
+//   }
+//   render(){
+//           return <div>
+//                      <h1>{this.state.message}</h1>
+//                   <button onClick={this.clickHandler.bind(this)}>click me</button>
+//                 </div>  
+//             }
+// }
+// export default Greet;
 
+
+//  ================Second Methode===========================================
+// class Greet extends React.Component {
+//          constructor(){
+//            super()
+//            this.state={
+//              message:'fisrt message'
+//            }
+//          }
+//   clickHandler(){
+//     this.setState({
+//       message:'last Message'
+//     })
+//   }
+//   render(){
+//           return <div>
+//                      <h1>{this.state.message}</h1>
+//                   <button onClick={()=>this.clickHandler()}>click me</button>
+//                 </div>  
+//             }
+// }
+// export default Greet;
+
+
+
+ //  ================thirth Methode===========================================
+// class Greet extends React.Component {
+//          constructor(){
+//            super()
+//            this.state={
+//              message:'fisrt message'
+//            }
+//            this.clickHandler=this.clickHandler.bind(this)
+//          }
+//   clickHandler(){
+//     this.setState({
+//       message:'last Message'
+//     })
+//   }
+
+//   render(){
+//           return <div>
+//                      <h1>{this.state.message}</h1>
+//                   <button onClick={this.clickHandler}>click me</button>
+//                 </div>  
+//             }
+// }
+// export default Greet;
+
+
+
+//  ================fourth Methode===========================================
 class Greet extends React.Component {
-
-  clickHandler(){
-    console.log('click from React Class');
+         constructor(){
+           super()
+           this.state={
+             message:'fisrt message'
+           }
+         
+         }
+  clickHandler=()=>{
+    this.setState({
+      message:'last Message'
+    })
   }
 
   render(){
-              return <div>
-                          <button onClick={this.clickHandler}>Class click me</button>
-                    </div>  
+          return <div>
+                     <h1>{this.state.message}</h1>
+                  <button onClick={this.clickHandler}>click me</button>
+                </div>  
             }
 }
 export default Greet;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
