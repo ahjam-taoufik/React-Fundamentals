657f2447e62a483e10901bc4db9707cbdb994e12
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-06 22:12:24 +0100

    List Rendering with key

---
+++ src/components/Person.js
@@ -0,0 +1,11 @@
+
+
+
+const Person = ({person}) => {
+   
+    return ( <li>  {person.name} </li>
+          
+     );
+}
+ 
+export default Person;
--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -1,20 +1,18 @@
+import Person from "./Person";
 
-const personsList=[
+const persons=[
     {id:1, name:'taoufik',skill:'React'},
     {id:2, name:'ahmed',skill:'Angular'},
     {id:3, name:'fatima',skill:'Vue'},
 ]
 
-let persons= personsList.map((person)=><h1>{person.name}</h1>)
+// let persons= personsList.map((person)=><h1>{person.name}</h1>)
 
 
 const PersonList = () => {
       
-    return ( 
-        <div>
-            {persons}
-        </div>
-     );
+    const personList=  persons.map( person => <Person key={person.id} person={person}  />)
+    return ( <ul> {personList} </ul>);
 }
 
 export default PersonList;
