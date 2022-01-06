70ca1b0fab120fef71f210c25758b937a3461b06
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-06 17:02:44 +0100

    callback in setState

--- src/components/Message.js
+++ src/components/Message.js
@@ -11,18 +11,27 @@ class Message extends Component {
              }
        }
 
-      funcHandler(a) {
-        this.setState({
-            message:'thank you ',
-            count:a
-        })
+      funcHandler() {
+        //   this.state.count=this.state.count+1;
+        
+        this.setState(
+             {
+            count:this.state.count+1,
+        },()=>{
+            console.log('callback function: ',this.state.count);
+             }
+
+             )
+           //  console.log('out of setstate1: ', this.state.count);
         }
+
+
     render(){
        
         return <div>
                    <h3>{this.state.message1}{this.state.count} {this.state.message2}</h3>
                    {/* <h1>{this.state.count}</h1> */}
-                   <button onClick={()=>this.funcHandler(this.state.count+1)}>click me</button>
+                   <button onClick={()=>this.funcHandler()}>click me</button>
               </div>
     }
 }
