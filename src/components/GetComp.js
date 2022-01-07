27c8e6e662e7db8ee93a1129f1246fcabcee8675
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-07 17:00:03 +0100

    HTTP GET

--- src/components/GetComp.js
+++ src/components/GetComp.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-
-
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class GetComp extends Component {
-     constructor(props) {
-         super(props)
-     
-         this.state = {
-              posts:[{
-                "id": 1, "title": "sunt aut fa1","body": "quia et suscipit\ns"},
-                {"id": 2, "title": "qui est esse2","body": "est rerum tempore vitil "}]
-         }
-     }
-     
+  constructor(props) {
+    super(props);
 
-    render() {
-        const {posts} =this.state
-      
-        console.log(posts);
-        return (
-            <div>
-             {posts.map(post=><h3>{post.title}</h3>)}
-            </div>
-        )
-    }
+    this.state = {
+      posts: [],
+      error:''
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts?_limit=10")
+      .then((res) => {this.setState({ posts: res.data,});})
+      .catch(err=>{
+             this.setState({
+                 error:'some thing were wrong'
+             })
+      })
+  }
+
+  render() {
+    const { posts,error } = this.state;
+
+    return (
+      <div>
+         <h2>you have {posts.length} posts in your database</h2>
+         <hr/>
+        {posts.length?posts.map((post) => (
+          <h4 key={post.id}>{post.title}</h4>)): error  }
+     
+      </div>
+    );
+  }
 }
